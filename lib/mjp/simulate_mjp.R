856e18a023f634bcfe3851a38cf502a8cda80e38
Arya Pourzanjani <arya@umail.ucsb.edu>
2015-06-23 11:40:49 -0700

    added working sampling function

--- lib/mjp/simulate_mjp.R
+++ lib/mjp/simulate_mjp.R
@@ -13,7 +13,22 @@ pi <- c(0.2, 0.2, 0.2, 0.2, 0.2)
 system.time(mjp <- rcfmc(1e8, Q, pi))
 
 #discreetly samples MJP up to time T with intervals
-#between samples uniformly selected from vector tau
-sample_mjp <- function(T, tau){
+#between samples uniformly selected from vector tau.
+sample_mjp <- function(T_stop, tauVec, mjp, nStates){
+  C <- array(0, c(nStates, nStates, length(tauVec)))
   
+  t <- 0
+  while(t <= T_stop){
+    tau <- sample(tau, size=1)
+    i <- mjp(t)
+    j <- mjp(t+tau)
+    
+    C[i,j,which(tauVec == tau)] <- C[i,j,which(tauVec == tau)] + 1
+    t <- t + tau
+  }
+  
+  return(C)
 }
+
+tauVec <- c(0.01, 0.1, 1)
+system.time(sample_mjp(3.7*1e6, tauVec, mjp, 5))
