de87b371c2d5053e62d9fb722a19d02503e1381c
Arya Pourzanjani <arya@umail.ucsb.edu>
2015-07-27 22:37:07 -0700

    added main pymc3 model file

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import pymc3 as pm
+
+K = 10
+D = 250
+
+model = pm.Model()
+with model:
+    Q_raw = DiscreteObsMJP_unif_prior('Q_raw', n=5, shape=(5,4))
+    
+    S = HMM_Blank('S')
+    C = DiscreteObsMJP('C', Q_raw=Q_raw, S=S, step_sizes=step_sizes)
+
+    B = Beta('B', )
+    X = Comorbidities('X', S=S, B=B, shape=(K, T_n))
+
+    Z = Beta('Z')
+    L = Beta('L')
+    O = Observations('O', X=X, Z=Z, L=L, shape=(D, T_n))
