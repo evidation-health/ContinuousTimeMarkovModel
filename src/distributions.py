5cf354d49fd074ca6375f97ee0bb9c6210243918
Arya Pourzanjani <arya@umail.ucsb.edu>
2015-08-11 12:05:51 -0700

    added support for multiple users for DiscreteObsMJP and Comorbidities

--- src/distributions.py
+++ src/distributions.py
@@ -2,10 +2,11 @@ from pymc3 import Continuous
 from pymc3.distributions.discrete import Categorical, Binomial
 from .transforms import rate_matrix
 import numpy as np
-import theano.tensor as T
+import theano.tensor as TT
 from theano.tensor.nlinalg import eig, matrix_inverse
 from theano.compile.sharedvalue import shared
 import theano.tensor.slinalg
+from theano.tensor.extra_ops import bincount
 
 class DiscreteObsMJP_unif_prior(Continuous):
 
@@ -15,19 +16,52 @@ class DiscreteObsMJP_unif_prior(Continuous):
         self.mode = Q
 
     def logp(self, value):
-        return T.as_tensor_variable(0)
+        return TT.as_tensor_variable(0)
 
 class DiscreteObsMJP(Continuous):
 
-    def __init__(self, pi, Q, observed_jumps, *args, **kwargs):
-        super(DiscreteObsMJP, self).__init__(dtype='int8',*args, **kwargs)
+    def __init__(self, pi, Q, M, N, observed_jumps, T, *args, **kwargs):
+        super(DiscreteObsMJP, self).__init__(dtype='int32',*args, **kwargs)
         self.pi = pi
         self.Q = Q
+        self.M = M
         self.observed_jumps = observed_jumps
-        self.step_sizes = np.unique(observed_jumps)
+        self.T = T
+        step_sizes = np.sort(np.unique(observed_jumps))
+        self.step_sizes = step_sizes = step_sizes[step_sizes > 0]
         self.mode = 0
 
+        #pad observed jumps with -1 for later use in computeC
+        self.N = N
+        max_obs = observed_jumps.shape[1]
+        obs_jumps = np.insert(observed_jumps, max_obs-1, -1, axis=1)
+        for n in range(N):
+        	obs_jumps[n,T[n]-1:] = -1
+
+        #convert observed jumps to their appropriate array index
+		obs_jump_ind = obs_jumps.copy()
+		for index, step in enumerate(step_sizes):
+		    obs_jump_ind[obs_jumps == step] = index
+		self.obs_jump_ind = obs_jump_ind
+
     def computeC(self,S):
+    	M = self.M
+    	n_step_sizes = len(self.step_sizes)
+
+    	obs_jump_ind = TT.as_tensor_variable(self.obs_jump_ind, 'obs_jump_ind')
+    	tau_ind = TT.flatten(obs_jump_ind)[:-1]*M*M
+    	keep_jumps = (tau_ind >= 0).nonzero()
+    	
+    	jump_from_ind = TT.flatten(S)[:-1]*M
+    	jump_to_ind = TT.flatten(S)[1:]
+
+    	flat_ind = (tau_ind + jump_from_ind + jump_to_ind)[keep_jumps]
+    	flat_ind_counts = bincount(flat_ind, minlength=n_step_sizes*M*M)
+
+    	C = flat_ind_counts.reshape(shape=np.array([n_step_sizes,M,M]))
+    	
+
+    	'''
     	Q = self.Q
         step_sizes = self.step_sizes
         observed_jumps = self.observed_jumps
@@ -54,6 +88,7 @@ class DiscreteObsMJP(Continuous):
             Cj = S[i+1]
             C = T.set_subtensor(C[Ci, Cj, tau_index], 
                                      C[Ci, Cj, tau_index]+1)
+		'''
         
         return C
         
@@ -68,25 +103,48 @@ class DiscreteObsMJP(Continuous):
         Q = self.Q
         step_sizes = self.step_sizes
 
-        Q_complex = T.cast(Q, 'complex64')
+
+        Q_complex = TT.cast(Q, 'complex64')
         C = self.computeC(S)
 
-        for i in range(0, len(step_sizes)):
+        n_step_sizes = len(self.step_sizes)
+        for i in range(0, n_step_sizes):
             #get P(tau)
             #lambdas, U = eig(Q_complex)
             tau = step_sizes[i]
-            P = T.slinalg.expm(tau*Q)
+            P = TT.slinalg.expm(tau*Q)
             #compute likelihood in terms of P(tau)
-            l += T.sum(C[:,:,i]*T.log(P))
+            l += TT.sum(C[i,:,:]*TT.log(P))
             
         return l
 
+from theano.compile.ops import as_op
+
+X_theano_type = TT.TensorType('int8', [False, False, False])
+@as_op(itypes=[TT.dscalar, TT.bscalar, TT.dmatrix, TT.dmatrix, X_theano_type, TT.imatrix, TT.lvector], otypes=[TT.lscalar])
+def logp_numpy(l,N,B0,B,X,S,T):
+	for n in xrange(N):
+		pX0 = np.prod(B0[X[:,0,n] == 1, S[n,0]]) * np.prod(1-B0[X[:,0,n] != 1, S[n,0]])
+		l += np.log(pX0)
+
+		for t in range(1,T[n]):
+			if S[n,t] != S[n,t-1]:
+				turned_on = (X[:,t-1,n] == 0 and X[:,t,n] == 1)
+				stayed_off = (X[:,t-1,n] == 0 and X[:,t,n] == 0)
+				l += np.log(np.prod(B[turned_on, S[n,t]]))
+				l += np.log(np.prod(1-B[stayed_off, S[n,t]]))
+
+		return l
+		#for t in range(1,T[n]):
+
 class Comorbidities(Continuous):
-    def __init__(self, S, B0, B, shape, *args, **kwargs):
+    def __init__(self, S, B0, B, T, shape, *args, **kwargs):
         super(Comorbidities, self).__init__(shape = shape, dtype='int8',*args, **kwargs)
         X = np.ones(shape)
         self.K = shape[0]
-        self.Tn = shape[1]
+        self.max_obs = shape[1]
+        self.N = shape[2]
+        self.T = T
         self.S = S
         self.B0 = B0
         self.B = B
@@ -94,20 +152,32 @@ class Comorbidities(Continuous):
 
     def logp(self, X):
         K = self.K
-        Tn = self.Tn
+        max_obs = self.max_obs
+        N = self.N
+        T = self.T
         S = self.S
         B0 = self.B0
         B = self.B
-        
-        l = 0.0
-        for k in range(K):
-            l += Binomial.dist(n=1, p=B0[k,S[0]]).logp(X[k,0])
+
+        l = np.float64(0.0)
+        #import pdb; pdb.set_trace()
+        l = logp_numpy(TT.as_tensor_variable(l),TT.as_tensor_variable(N),B0,B,X,S,TT.as_tensor_variable(T))        
+        '''
+        for n in xrange(N):
+        	#likelihood of X0
+        	
+			
+        	l += TT.sum(Binomial.dist(n=1, p=B0[:,S[n,0]]).logp(X[:,0,n]))
             
-        for t in range(1,Tn):
-            for k in range(K):
-                if T.eq(S[t],S[t-1]) or X[k,t-1] == 1:
-                    l += 0.0
-                else:
-                    l += Binomial.dist(n=1, p=B[k,S[t]]).logp(X[k,t])
+	        for t in range(1,T[n]):
+	            if TT.eq(S[n,t],S[n,t-1]):
+	            	l += 0.0
+	            else:
+	            	turned_on = (X[:,t-1,n] == 1).nonzero()
+	            	stayed_off = (X[:,t-1,n] != 1).nonzero()
+	            	l += TT.sum(Binomial.dist(n=1, p=B[turned_on,S[n,t]]).logp(X[turned_on,t,n]))
+	            	l += TT.sum(Binomial.dist(n=1, p=B[stayed_off,S[n,t]]).logp(X[stayed_off,t,n]))
+	    '''
+
         
         return l
--- src/main.py
+++ src/main.py
@@ -4,23 +4,17 @@ from ContinuousTimeMarkovModel.src.distributions import *
 from pymc3 import Model, sample, Metropolis, Dirichlet, Binomial, Beta
 from ContinuousTimeMarkovModel.src.forwardS import *
 
+N = 100
 M = 6
 K = 10
 D = 250
-Tn = 20
+min_obs = 10
+max_obs = 30
 
-observed_jumps = np.array([2,4,2,2,4,2,2,2,1,2,2,2,4,1,1,1,1,4,2])
-S = np.array([0,1,1,2,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5])
-X = np.array([[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=np.int8)
+from pickle import load
+T = load(open('../data/X_layer_100_patients/T.pkl', 'rb'))
+obs_jumps = load(open('../data/X_layer_100_patients/obs_jumps.pkl', 'rb'))
+X = load(open('../data/X_layer_100_patients/X_input.pkl', 'rb'))
 
 model = Model()
 
@@ -28,12 +22,14 @@ with model:
     pi = Dirichlet('pi', a = as_tensor_variable([0.5, 0.5, 0.5, 0.5, 0.5, 0.5]), shape=M)
     Q = DiscreteObsMJP_unif_prior('Q', M=M, shape=(M,M))
     
-    S = DiscreteObsMJP('S', pi=pi, Q=Q, observed_jumps=observed_jumps, shape=(Tn))
+    S = DiscreteObsMJP('S', pi=pi, Q=Q, M=M, N=N, observed_jumps=obs_jumps, T=T, shape=(N,max_obs))
 
     B0 = Beta('B0', alpha = 1, beta = 1, shape=(K,M))
     B = Beta('B', alpha = 1, beta = 1, shape=(K,M))
 
-    Xobs = Comorbidities('Xobs', S=S, B0=B0,B=B, shape=(K, Tn), observed = X)
+    Xobs = Comorbidities('Xobs', S=S, B0=B0,B=B, T=T, shape=(K, max_obs, N), observed = X)
+
+    import pdb; pdb.set_trace()
 
     #Z = Beta('Z')
     #L = Beta('L')
@@ -45,19 +41,20 @@ Q_raw_log = np.log(np.array([[0.631921, 0.0000001, 0.0000001, 0.0000001, 0.00000
                              [0.0000001, 0.0000001, 0.0000001, 0.206042, 0.0000001],
                              [0.0000001, 0.0000001, 0.0000001, 0.0000001, 0.609582],
                              [0.0000001, 0.0000001, 0.0000001, 0.0000001, 0.0000001]]))
-
+'''
 Q_raw_log = np.log(np.array([[1, 0.0000001, 0.0000001, 0.0000001, 0.0000001], 
                              [0.0000001, 1, 0.0000001, 0.0000001, 0.0000001],
                              [0.0000001, 0.0000001, 1, 0.0000001, 0.0000001],
                              [0.0000001, 0.0000001, 0.0000001, 1, 0.0000001],
                              [0.0000001, 0.0000001, 0.0000001, 0.0000001, 1],
                              [0.0000001, 0.0000001, 0.0000001, 0.0000001, 0.0000001]]))
+'''
 
 start = {'Q_ratematrix': Q_raw_log}
 
 with model:
     step1 = Metropolis(vars=[pi,Q])
-    step2 = ForwardS(vars=[S], X=X, observed_jumps=observed_jumps)
+    step2 = ForwardS(vars=[S], X=X, observed_jumps=obs_jumps)
     step3 = Metropolis(vars=[B0,B])
     trace = sample(300, [step1, step2, step3], start=start)
 
