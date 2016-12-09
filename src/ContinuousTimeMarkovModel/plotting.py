0db61e48cdf32c78a5ab6c91f0802493a970230f
dstuck <dstuck88@gmail.com>
2016-12-08 21:55:22 -0800

    Add files from laptop

--- src/ContinuousTimeMarkovModel/plotting.py
+++ src/ContinuousTimeMarkovModel/plotting.py
@@ -3,9 +3,6 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
-
 def groundTruthTraceplot(truths,trace,var,sigma=0.01,scale=None,jitter=0.01,ymax=None,show=True,**kwargs):
     ax = traceplot(trace=trace,vars=[var],**kwargs)
 #Reset color cycle to match colors properly
@@ -20,8 +17,6 @@ def groundTruthTraceplot(truths,trace,var,sigma=0.01,scale=None,jitter=0.01,ymax
     for truth in truths:
         randj = (np.random.rand()-0.5)*jitter
         ax[0][0].plot(x,mlab.normpdf(x,truth+randj,sigma),'--')
-        #ax[0][0].plot(x,mlab.normpdf(x,truth,sigma)+(np.random.rand()-0.5)*jitter,'--')
-        #ax[0][0].plot(x,gaussian(x,truth,sigma))
     if ymax is not None:
         ax[0][0].set_ylim((0,ymax))
     if show:
